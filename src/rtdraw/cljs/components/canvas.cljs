95279c62e84c55a483d34cae7b1ef686836f5a66
Earther <qn.khuat@gmail.com>
2021-10-18 22:37:38 +0700

    we got it drawing baby

--- src/rtdraw/cljs/components/canvas.cljs
+++ src/rtdraw/cljs/components/canvas.cljs
@@ -8,75 +8,79 @@
 
 ;(defn canvas
 ;  []
-;  (let [ch (chan (dropping-buffer 1024))
-;        this (atom nil)
-;        drawing (r/atom false)
-;
-;        handle-mouse-down 
-;        (fn [e] (put! ch {:type :mouse-down, :x (.-clientX e), :y (.-clientY e)}))
-;
-;        handle-mouse-up 
-;        (fn [e] (put! ch {:type :mouse-up, :x (.-clientX e), :y (.-clientY e)}))
-;
-;        handle-mouse-move 
-;        (fn [e] (put! ch {:type :mouse-move, :x (.-clientX e), :y (.-clientY e)}))
-;
-;        handle-on-click
-;        (fn [e] (do
-;                  (js/console.log "click ne: " (.getContext @this "2d"))
-;                  (set! (.. (.getContext @this "2d") "red") -fillStyle "red")
-;                  (.rect (.getContext @this "2d") 100 100 50 30)
-;                  ;(.fill (.getContext @this "2d"))
-;                  ()))
-;
-;        ]
-;    (r/create-class
-;      {
-;       :component-did-mount
-;       (fn []
-;         (go-loop [msg (<! ch)]
-;                  (match [msg]
-;                         [{:type :mouse-move, :x x, :y y}]
-;                         (when (and @this @drawing) 
-;                           (let [ctx (.getContext @this "2d")]
-;                             (js/console.log "x: " x, ", y: " y)
-;                             ;(.lineTo ctx x y)
-;                             ;(.stroke ctx)
-;                             ;(.beginPath ctx)
-;                             ;(.moveTo ctx x y)
-;                             (.rect ctx x y 5 5)
-;                             )
-;                           )
-;
-;
-;                         [{:type :mouse-up, :x _, :y _}]
-;                         (do 
-;                           (reset! drawing false)
-;                           ;(when @this 
-;                           ;  ;(.beginPath (.getContext @this "2d"))
-;                           ;  )
-;                           )
-;
-;                         [{:type :mouse-down, :x x, :y y}]
-;                         (do 
-;                           (reset! drawing true)
-;                           (>! ch {:type :mouse-move, :x x, :y y})
-;                           )
-;
-;                         :else
-;                         (js/console.log "what????????????"))
-;                  (recur (<! ch))
-;                  ))
-;
-;       :reagent-render 
-;       (fn [] 
-;         [:canvas
-;          {:class "w-screen h-screen"
-;           ;:on-mouse-down handle-mouse-down
-;           ;:on-mouse-up handle-mouse-up
-;           ;:on-mouse-move handle-mouse-move
-;           :on-click handle-on-click 
-;           :ref (fn [el] (reset! this el))
-;           }]
-;         )}
-;      )))
+;  [:canvas {
+;            :on-click (fn [e]
+;                        (let [el (.getElementById js/document "canvas")
+;                              ctx (.getContext el "2d")]
+;                          (js/console.log "canvas" el)
+;                          (js/console.log "context" ctx)
+;                          ;(set! (.-fillStyle ctx) "red")
+;                          (.fillRect ctx 0 0 100 100)
+;                          )
+;                        )
+;            :id "canvas" :class "w-screen h-screen"}]
+;  )
+
+(defn canvas
+  []
+  (let [ch (chan (dropping-buffer 1024))
+        this (atom nil)
+        drawing (r/atom false)
+
+        handle-mouse-down 
+        (fn [e] (put! ch {:type :mouse-down, :x (.-clientX e), :y (.-clientY e)}))
+
+        handle-mouse-up 
+        (fn [e] (put! ch {:type :mouse-up, :x (.-clientX e), :y (.-clientY e)}))
+
+        handle-mouse-move 
+        (fn [e] (put! ch {:type :mouse-move, :x (.-clientX e), :y (.-clientY e)}))
+
+        ]
+    (r/create-class
+      {
+       :component-did-mount
+       (fn []
+         (set! (.. (.getContext @this "2d") -canvas -width) (.-innerWidth js/window))
+         (set! (.. (.getContext @this "2d") -canvas -height) (.-innerHeight js/window))
+         (go-loop [msg (<! ch)]
+                  (match [msg]
+                         [{:type :mouse-move, :x x, :y y}]
+                         (when (and @this @drawing) 
+                           (let [ctx (.getContext @this "2d")]
+                             (.lineTo ctx x y)
+                             (.stroke ctx)
+                             (.beginPath ctx)
+                             (.moveTo ctx x y)))
+
+                         [{:type :mouse-up, :x _, :y _}]
+                         (do 
+                           (reset! drawing false)
+                           (when @this 
+                             (.beginPath (.getContext @this "2d"))
+                             )
+                           )
+
+                         [{:type :mouse-down, :x x, :y y}]
+                         (do 
+                           (reset! drawing true)
+                           (>! ch {:type :mouse-move, :x x, :y y})
+                           )
+
+                         :else
+                         (js/console.log "what????????????"))
+                  (recur (<! ch))
+                  ))
+
+       :reagent-render 
+       (fn [] 
+         [:canvas
+          {:class "w-screen h-screen"
+           :on-mouse-down handle-mouse-down
+           :on-mouse-up handle-mouse-up
+           :on-mouse-move handle-mouse-move
+           ;:on-click handle-on-click 
+           :ref (fn [el] (reset! this el))
+           }]
+         )}
+      )))
