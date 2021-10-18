f7e905a1b690fda4c654ae94140677d99b5aa4e9
Earther <qn.khuat@gmail.com>
2021-10-18 13:36:13 +0700

    add reaect and tailwindcss

--- src/rtdraw/cljs/main.cljs
+++ src/rtdraw/cljs/main.cljs
@@ -1,4 +1,16 @@
-(ns rtdraw.cljs.main)
+(ns rtdraw.cljs.main
+  (:require [reagent.core :as r]
+            [reagent.dom :as rd]))
 
 (defn init []
-  (println "Hello World"))
+  (println "Hello World!!!!!!!")
+  (println "yay")
+  )
+
+(def current-count (r/atom 0))
+
+(defn Application []
+  [:h2 {:class "text-red-400 mt-6 text-center text-3xl font-extrabold"} "Sign in to your account"]
+  )
+
+(rd/render [Application] (js/document.getElementById "root"))
