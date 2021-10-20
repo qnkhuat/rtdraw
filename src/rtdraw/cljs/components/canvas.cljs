ae5271a9a04a6d87316521d4f575f3f9f29a3b0b
Earther <qn.khuat@gmail.com>
2021-10-20 12:28:23 +0700

    we have change size and color

--- src/rtdraw/cljs/components/canvas.cljs
+++ src/rtdraw/cljs/components/canvas.cljs
@@ -1,6 +1,6 @@
 (ns rtdraw.cljs.components.canvas
   (:require [reagent.core :as r]
-            [rtdraw.cljs.components.mui :refer [Button]]
+            [rtdraw.cljs.components.mui :refer [Button FormControl MenuItem Select InputLabel Slider]]
             [cljs.core.match :refer [match]]
             [clojure.edn :as edn]
             [cljs.core.async :as a :refer [put! >! <! go go-loop dropping-buffer chan]]
@@ -13,18 +13,27 @@
     {:x (- (.-clientX e) (.-left rect))
      :y (- (.-clientY e) (.-top rect))}))
 
+
+(defn get-ctx
+  [ref]
+  (when @ref
+    (.getContext @ref "2d")))
+
+(defn is-drawing
+  [state]
+  (:drawing @state))
+
 (defn canvas
   []
   (let [ch (chan (dropping-buffer 1024))
         conn (js/WebSocket.  "ws://localhost:3000/ws/")
 
-        state (r/atom {:pen-type :pencil
+        state (r/atom {:pen-type :pen
                        :color "black"
                        :stroke-size 5
+                       :drawing false ; keeping track of whether or not is drawing mode
                        })
-
-        this (atom nil)
-        drawing (r/atom false)
+        this (atom nil) 
 
         handle-mouse-down 
         (fn [e] 
@@ -38,13 +47,14 @@
         (fn [e] 
           (put! ch (merge {:type :mouse-move} (get-mouse-pos e @this))))
 
-
-        handle-msg
+        handle-draw
         (fn [msg]
           (match [msg]
                  [{:type :mouse-move, :x x, :y y}]
-                 (when (and @this @drawing) 
+                 (when (and @this (is-drawing state)) 
                    (let [ctx (.getContext @this "2d")]
+                     (set! (.. ctx -lineWidth) (:stroke-size @state))
+                     (set! (.. ctx -strokeStyle) (:color @state))
                      (.lineTo ctx x y)
                      (.stroke ctx)
                      (.beginPath ctx)
@@ -54,18 +64,33 @@
 
                  [{:type :mouse-up, :x _, :y _}]
                  (do 
-                   (reset! drawing false)
+                   (swap! state assoc :drawing false)
                    (when @this 
                      (.beginPath (.getContext @this "2d"))))
 
                  [{:type :mouse-down, :x x, :y y}]
                  (do 
-                   (reset! drawing true)
+                   (swap! state assoc :drawing true)
                    (put! ch {:type :mouse-move, :x x, :y y}))
 
                  :else
                  (js/console.error "????????????"))
-          )]
+
+          )
+        handle-change-pen
+        (fn [e]
+          (swap! state assoc :pen-type (keyword (.. e -target -value))))
+
+        handle-change-stroke-size
+        (fn [e]
+          (swap! state assoc :stroke-size (.. e -target -value))
+          )
+
+        handle-change-color
+        (fn [e]
+          (swap! state assoc :color (.. e -target -value)))
+
+        ]
     (r/create-class
       {
        :component-did-mount
@@ -75,21 +100,50 @@
          (set! (.. @this -height) (.-offsetHeight @this))
 
          (set! (.-onmessage conn) (fn [msg] 
-                                    (handle-msg (->> msg .-data edn/read-string))
+                                    (handle-draw (->> msg .-data edn/read-string))
                                     ))
 
          ; loop to draw
          (go-loop [msg (<! ch)]
+                  (handle-draw msg)
                   (when (= 1 (.-readyState conn))
-                    (.send conn msg)
-                    (handle-msg msg))
+                    (.send conn msg))
                   (recur (<! ch))
                   ))
 
        :reagent-render 
        (fn [] 
-         [:div
-          [Button "click me bitch"]
+         [:div {:class "mt-24 mx-12"}
+          [FormControl
+           [InputLabel {:id "select-pen"} "Pen"]
+           [Select {:labelId "select-pen"
+                    :id "select"
+                    :value (:pen-type @state)
+                    :onChange handle-change-pen
+                    }
+            [MenuItem {:value :rectangle} "Rect"]
+            [MenuItem {:value :circle} "Circle"]
+            [MenuItem {:value :pen} "Pen"]
+            ]
+           ]
+
+          [FormControl
+           [InputLabel {:id "select-color"} "color"]
+           [Select {:labelId "select-color"
+                    :id "select"
+                    :value (:color @state)
+                    :onChange handle-change-color
+                    }
+            [MenuItem {:value "black"} "Black"]
+            [MenuItem {:value "red"} "Red"]
+            [MenuItem {:value "blue"} "Blue"]
+            ]
+           ]
+          [Slider {:aria-label "Stroke size" 
+                   :min 1
+                   :max 20
+                   :value (:stroke-size @state)
+                   :onChange handle-change-stroke-size}]
           [:canvas
            {:class "mt-24 ml-24 border-2 border-black w-screen h-screen"
             :on-mouse-down handle-mouse-down
--- src/rtdraw/cljs/components/mui.cljs
+++ src/rtdraw/cljs/components/mui.cljs
@@ -1,6 +1,11 @@
 (ns rtdraw.cljs.components.mui
   (:require [reagent.core :as r]
             ["@mui/material/Button" :as MuiButton]
+            ["@mui/material/FormControl" :as MuiFormControl]
+            ["@mui/material/InputLabel" :as MuiInputLabel]
+            ["@mui/material/MenuItem" :as MuiMenuItem]
+            ["@mui/material/Select" :as MuiSelect]
+            ["@mui/material/Slider" :as MuiSlider]
             ))
 
 (defn -adapt
@@ -8,3 +13,8 @@
   (r/adapt-react-class (.-default component)))
 
 (def Button (-adapt MuiButton))
+(def FormControl (-adapt MuiFormControl))
+(def InputLabel (-adapt MuiInputLabel))
+(def MenuItem (-adapt MuiMenuItem))
+(def Select (-adapt MuiSelect))
+(def Slider (-adapt MuiSlider))
